bcabf5cfb9ebf470bb6e8c62d6ee0671abcfab3d
Malashree Dhungel <malashree.dhungel@coyotes.usd.edu>
2025-04-18 14:00:40 -0500

    added timer

--- mini-sudoku-game/src/App.tsx
+++ mini-sudoku-game/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import SudokuGrid from './components/SudokuGrid';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
       <h1>Mini Sudoku</h1>
+      <Timer />
       <SudokuGrid />
     </div>
   );
